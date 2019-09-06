3a49d7c4ef169174e90025744546c3b772814956
Grenek <grenek075@gmail.com>
2019-09-06 11:10:00 +0300

    search by ID works

--- src/components/detailedInfo.js
+++ src/components/detailedInfo.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { ListGroup, Card, Container } from 'react-bootstrap'
 import '../styles/style.scss';
 
 class DetailedInfo extends React.Component {
   constructor(props) {
     super(props)
+    // this.carsToShow = this.carsToShow.bind(this)
     this.state = {
       id: 0,
       name: "",
@@ -19,20 +20,27 @@ class DetailedInfo extends React.Component {
     }
   }
 
-  getPersonData() {
-    axios
-      .get('http://172.30.215.172:8081/RESTfulWebApp/personwithcars', {
-        params: {
-          personid: 4
-        }
-      })
-      .then(response => {
-        this.setState({ ...response.data })
-      })
-  }
+  // getPersonData() {
+  //   axios
+  //     .get('http://172.30.215.172:8081/RESTfulWebApp/personwithcars', {
+  //       params: {
+  //         personid: 4
+  //       }
+  //     })
+  //     .then(response => {
+  //       this.setState({ ...response.data })
+  //     })
+  // }
 
-  componentDidMount() {
-    // this.getPersonData()
+  carsToShow() {
+    let cars = this.props.peopleWitchCars.cars
+    let carsList = []
+    if (typeof cars !== "undefined") {
+      carsList = cars.map((car) => 
+        <ListGroup.Item key={car.id}>{car.model}</ListGroup.Item>
+      )
+  }
+    return carsList
   }
 
   render() {
@@ -44,13 +52,7 @@ class DetailedInfo extends React.Component {
           <Card.Body>{this.props.peopleWitchCars.birthdate}</Card.Body>
         </Card>
         <ListGroup className="d-flex justify-content-end">
-          {/* {console.log(this.props.peopleWitchCars.cars)
-          } */}
-          
-          {this.props.peopleWitchCars.cars.map(car => {
-            return <ListGroup.Item key={car.id}>{car.model}</ListGroup.Item>
-          })}
-          
+          {this.carsToShow()}
         </ListGroup>
       </Container>
     )
--- src/components/searchOwner.js
+++ src/components/searchOwner.js
@@ -9,12 +9,20 @@ class SearchOwner extends React.Component {
     this.state = {}
   }
 
+  // смотрим что юзер ввел и записываем id в state
+  handleUserInput = (e) => {
+    const id = e.target.name;
+    const value = e.target.value;
+    this.setState({[id]: value});
+  }
+
+  //делаем запрос к API и записываем ответ в state
   searchOwner = (e) => {
     e.preventDefault();
     axios
     .get('http://172.30.215.172:8081/RESTfulWebApp/personwithcars', {
       params: {
-        personid: 4
+        personid: this.state.id
       }
     })
     .then(response => {
@@ -29,7 +37,7 @@ class SearchOwner extends React.Component {
         <Form onSubmit={this.searchOwner}>
           <Form.Group>
             <Form.Label>Поиск автовладельца</Form.Label>
-            <Form.Control placeholder="Введите ID" />
+            <Form.Control placeholder="Введите ID" name="id" onChange={this.handleUserInput}/>
           </Form.Group>
         </Form>
       </div>
