1abb16dae04ae5ec03bfd2b37a33313ef13d7438
Grenek <grenek075@gmail.com>
2019-09-26 16:07:26 +0300

    styling start

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import '../styles/style.scss';
-import { Nav } from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import logo from '../images/car.png';
 
-class Navbar extends React.Component {
+class Navigation extends React.Component {
     render() {
         return (
-            <div className="navbar">
-                <Nav variant="pills">
-                    <LinkContainer to="/owners">
-                        <Nav.Link>Автовладельцы</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/cars">
-                        <Nav.Link>Автомобили</Nav.Link>
-                    </LinkContainer>
-<                   LinkContainer to="/statistics">
-                        <Nav.Link>Статистика</Nav.Link>
-                    </LinkContainer>
-                </Nav>
+            <div>
+                <Navbar bg="dark" variant="dark" sticky="top">
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={logo}
+                            width="40"
+                            height="40"
+                            className="d-inline-block align-top"
+                            alt="logo"
+                        />
+                    </Navbar.Brand>
+                    {/* <Navbar.Brand>Автокек</Navbar.Brand> */}
+                    <Nav variant="pills">
+                        <LinkContainer to="/owners">
+                            <Nav.Link>Автовладельцы</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/cars">
+                            <Nav.Link>Автомобили</Nav.Link>
+                        </LinkContainer>
+                        <                   LinkContainer to="/statistics">
+                            <Nav.Link>Статистика</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar>
             </div>
         )
     }
 }
 
-export default Navbar;
+export default Navigation;
